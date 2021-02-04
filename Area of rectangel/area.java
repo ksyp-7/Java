5bb1d8bf2207cd33de73a5ce21bb17a1f13e2802
shukl kashyap <shuklakashyap777777@gmail.com>
2021-02-04 17:54:38 +0530

    Area of rectngel

---
+++ Area of rectangel/area.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class area {
+    public static void main(String args[]){
+        Scanner s = new Scanner(System.in);
+        int l,b,a;
+		System.out.print("Enter length : ");
+		l=s.nextInt();
+		System.out.print("Enter bridth : ");
+		b=s.nextInt();
+		a=l*b;
+		System.out.print("\n Area of rectangle : "+ a);
+
+    }
+}
